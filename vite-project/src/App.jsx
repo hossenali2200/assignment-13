b9a1a5170b623618e39f25b312b64704a496a63e
hossenali2200 <hosenali2200@gmail.com>
2023-11-08 13:45:31 +0600

    assignment-13
    react vite project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,33 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
-import Home from './Home';
-import Service from './Service';
-import About from './About';
-import Project from './Project';
-import Blog from './Blog';
-import Contact from './Contact';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+
+import Header from './Home/Header';
+import Footer from './Home/Footer';
+import Content from './Home/Content';
+import Card from './Home/Card.jsx';
+ 
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <NavLink to="/" activeClassName="active">Home</NavLink>
-        <NavLink to="./service" activeClassName="active">Service</NavLink>
-        <NavLink to="./about" activeClassName="active">About</NavLink>
-        <NavLink to="./project" activeClassName="active">Project</NavLink>
-        <NavLink to="./blog" activeClassName="active">Blog</NavLink>
-        <NavLink to="./contact" activeClassName="active">Contact</NavLink>
-      </nav>
-      <Switch>
-        <Route path="./service" component={Service} />
-        <Route path="./about" component={About} />
-        <Route path="./project" component={Project} />
-        <Route path="./blog" component={Blog} />
-        <Route path="./contact" component={Contact} />
-        <Route path="./" component={Home} />
-      </Switch>
-    </Router>
-  );
+     
+      <div>
+        <Header/> 
+        
+        <Content/> 
+
+        <Card/>
+
+        <Footer/> 
+         
+
+  
+      </div>
+       
+   );
 }
+   
 
 export default App;
